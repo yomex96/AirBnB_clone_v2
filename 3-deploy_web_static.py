50a97a343da7117c0a4ee4e4109d29c91822b90e
Benjamin <benjminrichie@gmail.com>
2024-03-18 12:14:58 -0700

    insight

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,7 +11,7 @@ env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
+    """To create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
                                                          dt.month,
