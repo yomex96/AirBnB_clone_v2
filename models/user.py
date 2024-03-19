31a4e51bcb40d732394c863f7a346d8bbcb2ae81
yomex96 <abayomirobertonawole2@gmail.com>
2024-03-19 02:01:44 -0700

    weal

--- models/user.py
+++ models/user.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""This represents the user class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+"""This module defines a class User"""
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
+from hashlib import md5
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+    """This class defines a user by various attributes"""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place",
+                              backref="User",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review",
+                               backref="User",
+                               cascade="all, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
