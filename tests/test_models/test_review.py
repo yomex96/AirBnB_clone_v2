922a6a2aa92be9cf34f5973412407efc4c0313af
yomex96 <abayomirobertonawole2@gmail.com>
2024-03-20 01:12:03 -0700

    weston

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+from os import getenv
 
 
 class test_review(test_basemodel):
@@ -16,14 +17,23 @@ class test_review(test_basemodel):
     def test_place_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        if getenv("HBNB_TYPE_STORAGE") == "db":
+            self.assertEqual(new.place_id, None)
+        else:
+            self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        if getenv("HBNB_TYPE_STORAGE") == "db":
+            self.assertEqual(new.user_id, None)
+        else:
+            self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.text), str)
+        if getenv("HBNB_TYPE_STORAGE") == "db":
+            self.assertEqual(new.text, None)
+        else:
+            self.assertEqual(type(new.text), str)
