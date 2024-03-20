4fe1a4b8ee28cff3912b6b2f48d2d9d95d1c3d5c
yomex96 <abayomirobertonawole2@gmail.com>
2024-03-20 02:37:07 -0700

    resillient

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""This starts a Flask web application.
-
-This application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-"""
+"""Start Flask web App"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return value"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'."""
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hello_HBNB():
+    """Return value"""
+    return 'HBNB'
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port="5000")
