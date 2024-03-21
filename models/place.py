31e63385249cf7799ad3ddd3413b33332be2cc30
yomex96 <abayomirobertonawole2@gmail.com>
2024-03-20 23:20:18 -0700

    pen

--- models/place.py
+++ models/place.py
@@ -79,5 +79,5 @@ class Place(BaseModel, Base):
 
         @amenities.setter
         def amenities(self, value):
-            if type(value) == Amenity:
+            if isinstance(value, Amenity):
                 self.amenity_ids.append(value.id)
