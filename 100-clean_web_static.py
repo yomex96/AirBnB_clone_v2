6796e997aaff84e5a5a7bee453c4a4c52a01c56c
yomex96 <abayomirobertonawole2@gmail.com>
2024-04-04 01:34:01 -0700

    finr

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,23 +11,27 @@ from fabric.api import *
 env.hosts = ['54.173.104.114', '34.227.94.64']
 
 
-def do_clean(number=0):
-    """Delete out-of-date archives.
+def do_clean(number=1):
+    """
+    Delete out-of-date archives.
+
     Args:
         number (int): The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
+            If number is 0 or 1, keeps only the most recent archive.
+            If number is 2, keeps the most and second-most recent archives, etc.
     """
-    number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    number = max(int(number), 1)
 
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        archives = run("ls -tr | grep 'web_static_'").split()
+        if len(archives) > number:
+            to_delete = archives[:-number]
+            run("rm -rf {}".format(" ".join(to_delete)))
+
+    with lcd("versions"):
+        local_archives = os.listdir(".")
+        local_archives = [archive for archive in local_archives if "web_static_" in archive]
+        if len(local_archives) > number:
+            to_delete_local = sorted(local_archives)[:-number]
+            [local("rm -rf {}".format(archive)) for archive in to_delete_local]
+
