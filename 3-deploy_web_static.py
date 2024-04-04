4d019334445419a06df4ff21098b5b5ab2d5d833
yomex96 <abayomirobertonawole2@gmail.com>
2024-04-04 16:43:27 -0700

    strength

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,52 +1,61 @@
 #!/usr/bin/python3
-import os
-import os.path
-from datetime import datetime
-from fabric.api import env, local, put, run
+""" 3 - deploy_web_static.py """
 
+from fabric.api import env, local, run
+from datetime import datetime
+
+# Define the Fabric environment variables
+env.user = 'your_username'
+env.key_filename = 'path_to_your_ssh_key'
 env.hosts = ['54.173.104.114', '34.227.94.64']
 
-def create_archive_name():
-    """Generates a unique name for the archive."""
-    current_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    return f"versions/web_static_{current_time}.tgz"
-
-def create_archive():
-    """Creates a tar gzipped archive of the directory web_static."""
-    archive_name = create_archive_name()
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-    if local(f"tar -cvzf {archive_name} web_static").failed:
+def do_pack():
+    """
+    Compresses the contents of the web_static folder into a .tgz archive
+    Returns: Path to the created archive if successful, None otherwise
+    """
+    try:
+        current_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+        archive_name = 'versions/web_static_{}.tgz'.format(current_time)
+        local('mkdir -p versions')
+        local('tar -czvf {} web_static'.format(archive_name))
+        return archive_name
+    except Exception as e:
+        print("An error occurred while creating the archive:", e)
         return None
-    return archive_name
 
-def distribute_archive(archive_path):
-    """Distributes an archive to a web server."""
-    if not os.path.isfile(archive_path):
+def do_deploy(archive_path):
+    """
+    Deploys the archive to the web servers
+    Args:
+        archive_path: Path to the archive to be deployed
+    Returns: True if successful, False otherwise
+    """
+    if archive_path is None:
         return False
-    file_name = os.path.basename(archive_path)
-    release_folder = f"/data/web_static/releases/{file_name.split('.')[0]}"
-    if put(archive_path, f"/tmp/{file_name}").failed:
+    try:
+        archive_name = archive_path.split('/')[-1]
+        archive_folder = '/data/web_static/releases/{}'.format(archive_name.split('.')[0])
+        run('mkdir -p {}'.format(archive_folder))
+        run('tar -xzf {} -C {}'.format(archive_path, archive_folder))
+        run('rm {}'.format(archive_path))
+        run('mv {}/web_static/* {}'.format(archive_folder, archive_folder))
+        run('rm -rf {}/web_static'.format(archive_folder))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(archive_folder))
+        print("New version deployed!")
+        return True
+    except Exception as e:
+        print("An error occurred while deploying the archive:", e)
         return False
-    commands = [
-        f"rm -rf {release_folder}",
-        f"mkdir -p {release_folder}",
-        f"tar -xzf /tmp/{file_name} -C {release_folder}",
-        f"rm /tmp/{file_name}",
-        f"mv {release_folder}/web_static/* {release_folder}/",
-        f"rm -rf {release_folder}/web_static",
-        "rm -rf /data/web_static/current",
-        f"ln -s {release_folder}/ /data/web_static/current"
-    ]
-    for command in commands:
-        if run(command).failed:
-            return False
-    return True
 
 def deploy():
-    """Creates and distributes an archive to web servers."""
-    archive_path = create_archive()
+    """
+    Orchestrates the deployment process
+    Returns: True if deployment successful, False otherwise
+    """
+    archive_path = do_pack()
     if not archive_path:
+        print("No archive created. Deployment aborted.")
         return False
-    return distribute_archive(archive_path)
-
+    return do_deploy(archive_path)
